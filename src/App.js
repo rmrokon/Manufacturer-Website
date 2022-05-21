33d9e28446054a6831016b92e5809bc537153344
rmrokon <mdrokon7773@gmail.com>
2022-05-21 22:54:31 +0600

    navbar done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
